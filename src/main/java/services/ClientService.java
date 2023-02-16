bc70c41b6cf7d382d56808d4957aaa48f1f9ae21
sergeyonischuk <sergey.onischuk@ukr.net>
2023-02-17 00:57:40 +0200

    try with resources in ClientService

--- src/main/java/services/ClientService.java
+++ src/main/java/services/ClientService.java
@@ -11,88 +11,97 @@ import java.util.List;
 
 public class ClientService {
 
-    public String getById(long id) throws SQLException {
-        if (validateInputId(id)) {
-            Connection con = DataSource.getConnection();
-            PreparedStatement st = con.prepareStatement("SELECT * FROM client WHERE client_id = ?");
-            st.setLong(1, id);
+    public String getById(long id) {
+        try (Connection con = DataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement("SELECT * FROM client WHERE client_id = ?")) {
+            if (validateInputId(id)) {
+                ps.setLong(1, id);
+                ResultSet rs = ps.executeQuery();
+                Client client = new Client();
+                while (rs.next()) {
+                    client.setId(id);
+                    client.setName(rs.getString("name"));
+                }
+                    return client.getName();
 
-            Client client = new Client();
-            ResultSet rs = st.executeQuery();
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public void setName(long id, String name) {
+        try (Connection con = DataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement("UPDATE client SET name = ? WHERE client_id = ?")) {
+            if (validateInputId(id) && validateInputName(name)) {
+                ps.setLong(2, id);
+                ps.setString(1, name);
+                ps.executeUpdate();
+            } else
+                throw new IllegalArgumentException("incorrect input ID or name parameters");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public long create(String name) {
+        try (Connection con = DataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement("INSERT INTO client (name) VALUES (?)")) {
+            if (validateInputName(name)) {
+                ps.setString(1, name);
+                ps.executeUpdate();
+                return getIdByName(name);
+            }
+            throw new IllegalArgumentException("incorrect name. Name must be not null, >= 2 and <= 1000 characters");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        throw new IllegalArgumentException();
+    }
+
+    public List<Client> listAll() {
+        try (Connection con = DataSource.getConnection(); PreparedStatement ps = con.prepareStatement("SELECT * FROM client")) {
+            ResultSet rs = ps.executeQuery();
+            List<Client> result = new ArrayList<>();
             while (rs.next()) {
-                client.setId(id);
-                client.setName(rs.getString("name"));
+                long id = rs.getLong("client_id");
+                String name = rs.getString("name");
+                result.add(new Client(id, name));
             }
-            if (client.getName() != null) {
-                return client.getName();
-            }
-            rs.close();
+            return result;
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-        throw new IllegalArgumentException("illegal id");
-
+        throw new IllegalArgumentException();
     }
 
-    public void setName(long id, String name) throws SQLException {
-        if (validateInputId(id) && validateInputName(name)) {
-            Connection con = DataSource.getConnection();
-            PreparedStatement st = con.prepareStatement("UPDATE client SET name = ? WHERE client_id = ?");
-            st.setLong(2, id);
-            st.setString(1, name);
-            st.executeUpdate();
-        } else
-            throw new IllegalArgumentException("incorrect input ID or name parameters");
-    }
-
-    public long create(String name) throws SQLException {
-        if (validateInputName(name)) {
-            Connection con = DataSource.getConnection();
-            PreparedStatement st = con.prepareStatement("INSERT INTO client (name) VALUES (?)");
-            st.setString(1, name);
-            st.executeUpdate();
-
-            return getIdByName(name);
+    public void deleteById(long id) {
+        try (Connection con = DataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement("DELETE FROM client WHERE client_id = ?")) {
+            if (validateInputId(id)) {
+                ps.setLong(1, id);
+                ps.executeUpdate();
+            } else
+                throw new IllegalArgumentException("illegal id");
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-        throw new IllegalArgumentException("incorrect name. Name must be not null, >= 2 and <= 1000 characters");
     }
 
-    public List<Client> listAll() throws SQLException {
-        Connection connection = DataSource.getConnection();
-        PreparedStatement st = connection.prepareStatement("SELECT * FROM client");
-        ResultSet rs = st.executeQuery();
-
-        List<Client> result = new ArrayList<>();
-
-        while (rs.next()) {
-            long id = rs.getLong("client_id");
-            String name = rs.getString("name");
-            result.add(new Client(id, name));
-        }
-        rs.close();
-        return result;
-    }
-
-    public void deleteById(long id) throws SQLException {
-        if (validateInputId(id)) {
-            Connection con = DataSource.getConnection();
-            PreparedStatement st = con.prepareStatement("DELETE FROM client WHERE client_id = ?");
-            st.setLong(1, id);
-            st.executeUpdate();
-        } else
-            throw new IllegalArgumentException("illegal id");
-    }
 
     private long getIdByName(String name) throws SQLException {
-        if (validateInputName(name)) {
-            Connection con = DataSource.getConnection();
-            PreparedStatement st = con.prepareStatement("SELECT * FROM client WHERE name = ?");
-            st.setString(1, name);
-            ResultSet rs = st.executeQuery();
-            while (rs.next()) {
-                return rs.getLong("client_id");
+        try (Connection con = DataSource.getConnection();
+             PreparedStatement ps = con.prepareStatement("SELECT * FROM client WHERE name = ?")) {
+            if (validateInputName(name)) {
+                ps.setString(1, name);
+                ResultSet rs = ps.executeQuery();
+                while (rs.next()) {
+                    return rs.getLong("client_id");
+                }
             }
-            rs.close();
+            throw new IllegalArgumentException("client with name " + name + " not found");
         }
-        throw new IllegalArgumentException("client with name " + name + " not found");
     }
 
     private boolean validateInputId(long id) {
